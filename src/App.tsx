a8c53e506379eac31250f87b7da9feadbd547f7f
Pushpinder Badoni <pushpinderhsp2023@gmail.com>
2025-05-11 03:21:37 -0400

    Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { Icon } from "@iconify/react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="bg-green-500">
-      App
-      <Icon icon="mdi-light:home" />
+    <div>
+      <Hero />
     </div>
   );
 };
