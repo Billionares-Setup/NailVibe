1b298ef71d17fa7db6863e6f83534d2eef0d7257
Pushpinder Badoni <pushpinderhsp2023@gmail.com>
2025-05-19 10:42:11 -0400

    Refactor Footer component: improve layout structure and update social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,26 +18,26 @@ const Footer = () => {
   return (
     <footer
       ref={footerRef}
-      className="bg-violet-500 z-50 py-16 px-8 md:px-24 border-t border-violet-800"
+      className="z-50 px-8 py-16 border-t bg-violet-500 md:px-24 border-violet-800"
     >
-      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-3 gap-6 text-left sm:text-left">
+      <div className="grid grid-cols-1 gap-6 mx-auto text-left max-w-7xl sm:grid-cols-3 sm:text-left">
         {/* Contact Info */}
         <div>
           <h3 className="text-xl font-semibold text-white">Contact Us</h3>
           <p
-            className="text-sm  text-white mt-1"
+            className="mt-1 text-sm text-white"
             style={{ fontFamily: '"Open Sans", sans-serif' }}
           >
             Royal Nails & Spa
           </p>
           <p
-            className="text-sm text-white mt-1"
+            className="mt-1 text-sm text-white"
             style={{ fontFamily: '"Open Sans", sans-serif' }}
           >
             161 Colborne St W, Brantford, ON N3T 1L2
           </p>
           <p
-            className="text-sm text-white mt-1"
+            className="mt-1 text-sm text-white"
             style={{ fontFamily: '"Open Sans", sans-serif' }}
           >
             Phone: +1(519) 304-9044
@@ -47,12 +47,12 @@ const Footer = () => {
         {/* Social Media */}
         <div className="flex flex-col items-start sm:items-center ">
           <h3 className="text-xl font-semibold text-white">Follow Us</h3>
-          <div className="flex justify-center gap-6 text-xl text-white mt-3">
+          <div className="flex items-center justify-center gap-4 mt-3 text-xl text-white">
             <a
-              href="https://www.facebook.com"
+              href="https://www.facebook.com/RoyalNailsandSpaBrantford"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-indigo-600 hover:text-black transition-colors"
+              className="text-indigo-600 transition-colors hover:text-black"
             >
               <Icon
                 icon="qlementine-icons:facebook-24"
@@ -60,12 +60,18 @@ const Footer = () => {
               />
             </a>
             <a
-              href="https://www.instagram.com"
+              href="https://www.instagram.com/royalnailsbrantford/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-indigo-600 hover:text-black transition-colors"
+              className="text-indigo-600 transition-colors hover:text-black"
             >
-              <Icon icon="mdi:instagram" className="text-white size-9" />
+              <Icon icon="hugeicons:instagram" className="text-white size-9" />
+            </a>
+            <a
+              href="mailto:royalnailsbrantford1@gmail.com"
+              className="text-indigo-600 transition-colors hover:text-black"
+            >
+              <Icon icon="mdi-light:email" className="text-white size-10" />
             </a>
           </div>
         </div>
@@ -74,25 +80,25 @@ const Footer = () => {
         <div>
           <h3 className="text-xl font-semibold text-white ">Hours</h3>
           <p
-            className="text-sm text-white mt-1"
+            className="mt-1 text-sm text-white"
             style={{ fontFamily: '"Open Sans", sans-serif' }}
           >
             Mon - Fri: 9am – 7pm
           </p>
           <p
-            className="text-sm text-white mt-1"
+            className="mt-1 text-sm text-white"
             style={{ fontFamily: '"Open Sans", sans-serif' }}
           >
             Sat: 9am – 6pm
           </p>
           <p
-            className="text-sm text-white mt-1"
+            className="mt-1 text-sm text-white"
             style={{ fontFamily: '"Open Sans", sans-serif' }}
           >
             Sun: Closed
           </p>
           <p
-            className="text-sm text-white mt-1"
+            className="mt-1 text-sm text-white"
             style={{ fontFamily: '"Open Sans", sans-serif' }}
           >
             © {toay.getFullYear()} Royal Nails & Spa
